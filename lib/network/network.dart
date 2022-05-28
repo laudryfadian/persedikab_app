944ddbe45cbb426214d816fd87c97c42eee22a22
laudryfadian <skakmate87@gmail.com>
2022-05-28 16:16:36 +0700

    add some endpoint

--- lib/network/network.dart
+++ lib/network/network.dart
@@ -1,8 +1,11 @@
 class BaseUrl {
-  static String domain = "http://localhost:8001/";
+  static String domain = "http://10.0.2.2:8000/";
 
   //auth
   static String register = domain + "user/register";
+  static String sendOtp = domain + "user/otp";
+  static String cekOtp = domain + "user/cekOtp";
+  static String login = domain + "user/login";
 
   //berita
   static String berita = domain + "berita";
