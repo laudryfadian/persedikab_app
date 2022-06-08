fc8a064baffb2fd0e48ef9fb050e4cc9eac4db7b
laudryfadian <skakmate87@gmail.com>
2022-06-08 22:47:41 +0700

    update news page

--- lib/pages/news/news_page.dart
+++ lib/pages/news/news_page.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:persedikab_app/models/news.dart';
+import 'package:http/http.dart' as http;
+import 'package:persedikab_app/network/network.dart';
+import 'package:persedikab_app/pages/news/news_datail.dart';
+import 'package:persedikab_app/widget/card_news.dart';
 
 class NewsPage extends StatefulWidget {
   const NewsPage({Key? key}) : super(key: key);
@@ -8,7 +15,36 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
+  List<Data> listNews = [];
   @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<List<Data>> _fetchData() async {
+    try {
+      var jsonResponse = await http.get(Uri.parse(BaseUrl.berita));
+      if (jsonResponse.statusCode == 200) {
+        var jsonItems = json
+            .decode(jsonResponse.body)['result']['data']
+            .cast<Map<String, dynamic>>();
+
+        listNews = jsonItems.map<Data>((json) {
+          return Data.fromJson(json);
+        }).toList();
+      }
+    } catch (e) {
+      print(e);
+    }
+    return listNews;
+  }
+
+  Future<Null> _refresh() {
+    return _fetchData().then((_listNews) {
+      setState(() => listNews = _listNews);
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
@@ -22,8 +58,43 @@ class _NewsPageState extends State<NewsPage> {
         )),
       ),
       body: Container(
-        child: Text('oke'),
-      ),
+          child: RefreshIndicator(
+        onRefresh: _refresh,
+        child: FutureBuilder<List<Data>>(
+          future: _fetchData(),
+          builder: (BuildContext context, AsyncSnapshot<List<Data>> snapshot) {
+            if (snapshot.hasData) {
+              listNews = snapshot.data!;
+              return Container(
+                height: double.infinity,
+                margin: EdgeInsets.only(bottom: 50),
+                child: ListView.builder(
+                    itemCount: listNews.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      var _data = listNews[index];
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      DetailNews(detail: _data)));
+                        },
+                        child: CardNews(news: _data),
+                      );
+                    }),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Colors.red,
+                ),
+              );
+            }
+          },
+        ),
+      )),
     );
   }
 }
